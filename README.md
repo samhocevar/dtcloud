b9eb6e578ea3e52ecb650cc054cd2255110de971
Sam Hocevar <sam@hocevar.net>
2016-05-23 20:57:04 +0200

    Add Makefile and improve stuff.

---
+++ README.md
@@ -0,0 +1,16 @@
+This is basically Cloud2Butt in French.
+
+# DTCloud #
+
+*Le vrai visage du Cloud*
+
+Vous en avez marre de lire « dans le cloud » toutes les trois phrases ? Remplacez
+facilement cette expression agaçante par « dans ton cul » sur toutes les pages que
+vous visitez grâce à cette extension !
+
+Pour Firefox : https://addons.mozilla.org/en-GB/firefox/addon/dtcloud/
+
+Pour Chrome : https://chrome.google.com/webstore/detail/dtcloud/ojmedhcbgjeopeclmpfifmhdbiiggflh
+
+Signaler un souci : sam@hocevar.net ou https://twitter.com/samhocevar
+
--- src/transform.js
+++ src/transform.js
@@ -1,20 +1,49 @@
-function walk(node)
+
+// Some stats to detect whether we’re dealing with French
+var is_french = false;
+
+var words = [ 'de', 'des', 'du', 'en', 'et', 'le', 'la', 'les', 'un', 'une' ];
+var letters = [ 'é', 'è', 'à', 'ù', 'ė' ];
+
+var word_count = {};
+var letter_count = {};
+
+for (var i in words)
+    word_count[words[i]] = 0;
+
+for (var i in letters)
+    letter_count[letters[i]] = 0;
+
+
+
+function walk(node, fun)
 {
     switch (node.nodeType)
     {
         case Node.TEXT_NODE:
-            transform(node);
+            fun(node);
             break;
         case Node.ELEMENT_NODE:
         case Node.DOCUMENT_NODE:
         case Node.DOCUMENT_FRAGMENT_NODE:
             for (var n = node.firstChild; n; n = n.nextSibling)
-                walk(n);
+                walk(n, fun);
             break;
         default:
             // Do nothing with unknown nodes
             break;
     }
+}
+
+function stats(node)
+{
+    var v = node.nodeValue.toLowerCase();
+
+    for (var i in words)
+        word_count[words[i]] += (v.match(new RegExp('\\b' + words[i] + '\\b', 'g')) || []).length;
+
+    for (var i in letters)
+        letter_count[letters[i]] += (v.match(new RegExp(letters[i], 'g')) || []).length;
 }
 
 function transform(node)
@@ -34,5 +63,17 @@ function transform(node)
         node.nodeValue = v;
 }
 
-walk(document.body);
+walk(document.body, stats);
+
+var good_words = 0;
+var good_letters = 0;
+for (var i in Object.keys(word_count))
+    if (word_count[words[i]] > 5)
+        ++good_words;
+for (var i in Object.keys(letter_count))
+    if (letter_count[letters[i]] > 5)
+        ++good_letters;
+
+if (good_words * 2 > words.length || good_letters * 2 > letters.length)
+    walk(document.body, transform);
 
